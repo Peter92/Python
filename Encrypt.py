d2e7b723533552905725a28d7bcc2aab5be7e99f
Peter <Peter92@users.noreply.github.com>
2015-02-20 04:47:21 +0000

    Update Encrypt.py
    Made it into a class

--- Encrypt.py
+++ Encrypt.py
@@ -1,56 +1,67 @@
-key = "hidden"
-input = "test"
+import md5, random, cPickle, base64, zlib
 
-import zlib, random, cPickle, base64
+class Encrypt:
+    
+    def __init__( self, key, keyLength = 128 ):
+        
+        #Set the random seed
+        m = md5.new()
+        m.update( key )
+        random.seed( m.digest() )
+        
+        #Generate new key from the random seed
+        newKey = ""
+        newKeyLength = keyLength
+        i = 0
+        while len( newKey ) < newKeyLength:
+            newKey += random.choice( str( abs( zlib.crc32( key[i%len( key )] ) ) ) )
+            i += 1
+        self.newKeyList = [( int( x )*random.randint( 0, 255 ) )%255 for x in list( newKey )]
+        self.newKeyListLength = len( self.newKeyList )
+        random.shuffle( self.newKeyList )
+        
+        #Decide how many extra characters to add for each character
+        originalKeyLength = len( key )
+        self.extraCharacters = [random.randint( 0, max( 1, int( originalKeyLength**0.5 ) ) ) for x in xrange( originalKeyLength+1 )]
+        self.charactersToAdd = [random.randint( 0, 255 ) for x in xrange( sum( self.extraCharacters ) + len( self.extraCharacters ) )]
+        self.extraCharactersLength = len( self.extraCharacters )
+        self.charactersToAddLength = len( self.charactersToAdd )
+        
+    def encode( self, input ):
+        
+        #Change letters based on values calculated from the key
+        i = 0
+        encodedString = ""
+        for j in range( len( input ) ):
+            
+            letterToChange = input[j]
+            characterNumber = ord( letterToChange )
+            
+            #Add extra letters so the word size is not so obvious
+            for k in xrange( self.extraCharacters[j%self.extraCharactersLength]+1 ):
+                encodedString += chr( ( characterNumber+self.newKeyList[i%self.newKeyListLength]+self.charactersToAdd[i%self.charactersToAddLength] )%255 )
+                i += 1
+        
+        return base64.b64encode( encodedString )
+        
+    def decode( self, input ):
+        
+        #Check string is in base64
+        try:
+            encodedString = base64.b64decode( input )
+        except:
+            print "Invalid input"
+            return None
+            
+        #Find the indexes where the original letters should be
+        extraCharactersIncrement = [self.extraCharacters[0]]
+        i = 0
+        while extraCharactersIncrement[-1] < len( encodedString ):
+            i += 1
+            extraCharactersIncrement.append( extraCharactersIncrement[-1]+self.extraCharacters[i%self.extraCharactersLength]+1 )
+        extraCharactersIncrement = extraCharactersIncrement[:-1]
+        
+        #Decode the string
+        decodedString = "".join( chr( ( ord( encodedString[i] )-self.newKeyList[i%self.newKeyListLength]-self.charactersToAdd[i%self.charactersToAddLength] )%255 ) for i in extraCharactersIncrement )
 
-#Set the random seed
-keyLength = len( key )
-keyCRC = zlib.crc32( key )
-splitKey = list( key )
-random.seed( keyCRC )
-
-#Generate new key from the random seed
-newKey = ""
-newKeyLength = 12
-i = 0
-while len( newKey ) < newKeyLength:
-    newKey += random.choice( str( abs( zlib.crc32( key[i%len( key )] ) ) ) )
-    i += 1
-newKeyList = [( int( x )*random.randint( 0, 255 ) )%255 for x in list( newKey )]
-random.shuffle( newKeyList )
-
-#Decide how many extra characters to add for each character
-extraCharacters = [random.randint( 0, max( 1, int( keyLength**0.5 ) ) ) for x in xrange( keyLength+1 )]
-charactersToAdd = [random.randint( 0, 255 ) for x in xrange( sum( extraCharacters ) + len( extraCharacters ) )]
-
-#Encode the characters using the key
-encodedString = ""
-increment = 0
-for i in range( len( input ) ):
-    letterToChange = input[i]
-    characterNumber = ord( letterToChange )
-    for j in xrange( extraCharacters[i%len( extraCharacters )]+1 ):
-        encodedString += chr( ( characterNumber+newKeyList[increment%len( newKeyList )]+charactersToAdd[increment%len( charactersToAdd )] ) % 255 )
-        increment += 1
-encodedString = base64.b64encode( encodedString )
-
-print encodedString
-
-#Decode the characters using the key
-input = encodedString
-try:
-    encodedString = base64.b64decode( input )
-except:
-    print "Invalid input"
-extraCharactersIncrement = [extraCharacters[0]]
-increment = 0
-while extraCharactersIncrement[-1] < len( encodedString ):
-    increment += 1
-    extraCharactersIncrement.append( extraCharactersIncrement[-1]+extraCharacters[increment%len( extraCharacters )]+1 )
-extraCharactersIncrement = extraCharactersIncrement[:-1]
-charactersToRemove = [charactersToAdd[i%len( charactersToAdd )]+newKeyList[i%len( newKeyList )] for i in extraCharactersIncrement]
-decodedString = ""
-for i in extraCharactersIncrement:
-    decodedString += chr( ( ord( encodedString[i] )-newKeyList[i%len( newKeyList )]-charactersToAdd[i%len( charactersToAdd )] )%255 )
-
-print decodedString
+        return decodedString
