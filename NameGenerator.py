3904139001006af59a92cdcb3e9cac35bdd4ddfd
Peter <Peter92@users.noreply.github.com>
2015-02-24 02:50:20 +0000

    Update NameGenerator.py
    Rewrote it from scratch, still at a basic stage

--- NameGenerator.py
+++ NameGenerator.py
@@ -1,21 +1,12 @@
-import math, random
+import math, random, zlib
 global letterGroup
-global wordSize
-global detail
-detail = 15.0
-letterGroup = {}
-try:
-    letterGroup["a"][0]
-except:
-    letterGroup = {}
-try:
-    wordSize
-except:
-    wordSize = {}
-    
+global accuracy
+accuracy = 15.0
+
 class LetterLoop:
+    letters = list( "abcdefghijklmnopqrstuvwxyz" )
+    
     def __init__( self, start = 0 ):
-        self.letters = list( "abcdefghijklmnopqrstuvwxyz" )
         self.currentNumber = start
         
     def next(self):
@@ -36,150 +27,150 @@ class LetterLoop:
         self.currentNumber += 1
         
         return result
-
-def addLetters( wordToUse, letterToUse, wordFrequency ):
-
-    letterIndex = [i for i, letter in enumerate( wordToUse ) if letter == letterToUse]
-    
-    wordLength = len( wordToUse )
-    
-    letterDetail = detail/wordLength
-    detailIndex = [[i*letterDetail,( i+1 )*letterDetail] for i in letterIndex]
-    
-    for i in detailIndex:
-        mainRange = range( int( math.floor( round( i[0], 5 ) ) ), int( math.ceil( round( i[1], 5 ) ) ) )
-        for j in mainRange:
-            startValue = i[0]
-            if j > i[0]:
-                startValue = j
-            endValue = int( startValue )+1
-            if endValue > i[1]:
-                endValue = i[1]
-            
-            letterGroup[letterToUse][int( startValue )] += ( endValue-startValue )*wordFrequency
-            
-            
-def iterateWords( wordList ):
-    
-    for letter in list( "abcdefghijklmnopqrstuvwxyz" ):
         
-        try:
-            letterGroup[letter]
-        except:
-            letterGroup[letter] = {}
-            
-        for i in range( int( detail ) ):
-            try:
-                letterGroup[letter][i]
-            except:
-                letterGroup[letter][i] = 0
-        
-        for words in wordList:
-            
-            wordLength = len( words[0] )
-            try:
-                wordSize[wordLength] += 1
-            except:
-                wordSize[wordLength] = 1
-            
-            addLetters( words[0], letter, words[1] )
-    
-    letterDetailFrequency = {}
-    for i in range( int( detail ) ):
-        letterDetailFrequency[i] = {}
-        for letter in list( "abcdefghijklmnopqrstuvwxyz" ):
-            letterDetailFrequency[i][letter] = letterGroup[letter][i]
-    
-    return letterDetailFrequency
-    
-
-def createNewWord( letterDetailFrequency, invalidRules = [], minWordSize = 0, maxWordSize = 100, newWordLength = -1 ):
-
-    newWord = []
-    
-    while not minWordSize <= newWordLength <= maxWordSize:
-        wordSizeSum = sum( frequency for size, frequency in wordSize.iteritems() )
-        randomNumber = random.uniform( 0, wordSizeSum )
-        frequencyCount = 0
-        for size in wordSize:
-            frequencyCount += wordSize[size]
-            if frequencyCount > randomNumber:
-                newWordLength = size
-                break
-            
-    maxMultiple = detail/newWordLength
-        
-    i = 0
-    while i < newWordLength:
-        
-        letterPosition = int( round( i*maxMultiple + maxMultiple/2.0 ) )
-        allLetters = letterDetailFrequency[ letterPosition ]
-        allLettersSum = sum( frequency for letter, frequency in allLetters.iteritems() )
-        
-        randomNumber = random.uniform( 0, allLettersSum )
-        frequencyCount = 0
-        for letter in allLetters:
-            frequencyCount += allLetters[letter]
-            if frequencyCount > randomNumber:
-                #Make sure 3 letters don't follow each other
-                if i > 2:
-                    if newWord[-1].lower() == letter and newWord[-2].lower() == letter:
-                        break
-                newWord.append( letter )
-                i += 1
-                break
-    
-    newWord = "".join( newWord )
-    if any( vowel in newWord for vowel in ["a","e","i","o","u"] ):
-        if not any( i in newWord for i in invalidRules ):
-            return newWord.title()
-    try:
-        return createNewWord( letterDetailFrequency, invalidRules, minWordSize, maxWordSize, len( newWord ) )
-    except:
-        pass
-
-
-def getInvalidCombinations( allWords, maxDepth = 3 ):
-    
-    
-    allLetters = list( "abcdefghijklmnopqrstuvwxyz" )
-    invalidCombinations = []
-    
-    maxNumber = 0
-    for i in range( maxDepth ):
-        maxNumber += 26**( maxDepth-i )
-            
-    for i in range( maxNumber ):
-        letterCombination = LetterLoop(i).next()
-        if letterCombination not in allWords:
-            invalidCombinations.append( letterCombination )
-    
-    return invalidCombinations
-
-#Need a file with lines set up as: word, frequency
+'''
 with open("C:/Code/words.csv") as f:
-    content = [i.replace("\r\n", "").split( "," ) for i in f.readlines()]
-    content = [[str( i[0] ), int( i[1] )] for i in content if i[1].isdigit()]
-letterDetailFrequency = iterateWords( content )
+    wordInput = set( word.replace("\r\n", "") for word in f.readlines() )
 
-#Need a file with lines set up as: word
+    wordList = [[str( splitWord[0] ), int( splitWord[1] )] if len( splitWord )>1 and splitWord[1].isdigit() else [str( splitWord[0] ), 1] for splitWord in wordInput]
+'''
+
 with open("C:/Code/wordsEn.txt") as f:
-    allWords = ",".join( [i.replace( "\r\n", "" ) for i in f.readlines()] )
+    wordInput = set( i.replace( "\r\n", "" ) for i in f.readlines() )
 
-
+#Build list into a dictionary
 try:
-    invalidCombinations
+    if wordInputHash != zlib.crc32( str( wordInput ) ):
+        raise ValueError()
 except:
-    maxDepth = 5
-    invalidCombinations = getInvalidCombinations( allWords, maxDepth )
+    wordList = {j[0]:int( j[1] ) if j[1] else 1 for j in [[j[0],0] if len( j ) == 1 else j for j in [i.split( "," ) for i in wordInput]]}
+    wordInputHash = zlib.crc32( str( wordInput ) )
 
-for i in range( 30 ):
-    '''
-    newWord = createNewWord( letterDetailFrequency, invalidCombinations, 3, random.randint( 7, 10 ) )
-    if newWord:
-        print newWord
-    '''
-    firstName = createNewWord( letterDetailFrequency, invalidCombinations, 3, random.randint( 5, 8 ) )
-    surname = createNewWord( letterDetailFrequency, invalidCombinations, 4, random.randint( 8, 12 ) )
-    if firstName and surname:
-        print "{0} {1}".format( firstName, surname )
+#Find range from a to z*(maxDepth)
+maxDepth = 3
+maxNumber = 0
+for i in xrange( maxDepth ):
+    maxNumber += 26**( maxDepth-i )
+
+#Calculate any combinates that don't exist in input word list
+try:
+    if lastMaxDepth != maxDepth:
+        raise ValueError()
+except:
+    allWordsJoined = ",".join( allWords )
+    invalidCombinations = set( j for j in set( LetterLoop( i ).next() for i in xrange( maxNumber ) ) if j not in allWordsJoined )
+    lastMaxDepth = maxDepth
+
+    #Iterate through each word to find the letter frequency
+    letterGroup = {letter: dict.fromkeys( xrange( int( accuracy ) ), 0 ) for letter in LetterLoop.letters}
+    letterDetailFrequency = {}
+    
+    for i in range( int( accuracy ) ):
+        letterDetailFrequency[i] = {}
+        
+    for wordToUse in wordList.keys():
+        wordFrequency = wordList[wordToUse]
+    
+        for letterToUse in LetterLoop.letters:
+    
+            #Find letter positions
+            letterIndex = [i for i, letter in enumerate( wordToUse ) if letter == letterToUse]
+            
+            #Average over range of 0 to accuracy
+            letterAccuracy = accuracy/len( wordToUse )
+            accuracyIndex = [[i*letterAccuracy,( i+1 )*letterAccuracy] for i in letterIndex]
+            
+            #Find frequency of letters being used
+            for i in accuracyIndex:
+                mainRange = range( int( math.floor( round( i[0], 5 ) ) ), int( math.ceil( round( i[1], 5 ) ) ) )
+                for j in mainRange:
+                    startValue = i[0]
+                    if j > i[0]:
+                        startValue = j
+                    endValue = int( startValue )+1
+                    if endValue > i[1]:
+                        endValue = i[1]
+                    letterGroup[letterToUse][int( startValue )] += ( endValue-startValue )*wordFrequency
+        
+        
+        #Calculate frequency of each letter for different points
+        for i in range( int( accuracy ) ):
+            for letter in LetterLoop.letters:
+                letterDetailFrequency[i][letter] = letterGroup[letter][i]
+
+
+    #Find average length of words:
+    allLengths = [len( word ) for word in allWords]
+    uniqueLengths = set( allLengths )
+    wordLenths = dict( ( i, allLengths.count( i ) ) for i in uniqueLengths )
+
+
+
+#Choose a length of a word
+wordLengthRandom = random.randint( 0, sum( wordLenths.values() ) )
+wordLengthCount = 0
+for newWordLength in wordLenths.keys():
+    wordLengthCount += wordLenths[newWordLength]
+    if wordLengthCount > wordLengthRandom:
+        break
+newWord = ""
+
+maxMultiple = accuracy/newWordLength
+totalLetters = 0
+
+#Figure out which characters to make word from
+letterAccuracy = accuracy/newWordLength
+for i in range( newWordLength ):
+    minValue = i*letterAccuracy
+    maxValue = (i+1)*letterAccuracy
+    minWholeValue = int( math.ceil( round( minValue, 5 ) ) )
+    maxWholeValue = int( math.floor( round( maxValue, 5 ) ) )
+    
+    
+    #Calculate what precision bands the letter falls under
+    valueRange = xrange( minWholeValue, maxWholeValue )
+    individualLetterValues = {i:1 if i in valueRange else 0 for i in xrange( int( accuracy ) )}    
+    if minValue < minWholeValue:
+        individualLetterValues[int( minValue )] = minValue-int( minValue )
+    if maxValue > maxWholeValue:
+        individualLetterValues[int( maxValue )] = maxValue-int( maxValue )
+    
+    #Count all letters
+    combinedLetterValues = {letter:0 for letter in LetterLoop.letters}
+    for i in individualLetterValues.keys():
+        for j in combinedLetterValues.keys():
+            combinedLetterValues[j] += letterDetailFrequency[i][j]*i
+    
+    #Build the word
+    maxRetries = 10
+    currentRetries = 0
+    
+    
+    
+    while True:
+        
+        breakMarker = None
+        randomSelection = random.uniform( 0, sum( combinedLetterValues.values() ) )
+        i = 0
+        
+        for letter in combinedLetterValues.keys():
+        
+            #Get letter
+            i += combinedLetterValues[letter]
+            
+            if i > randomSelection:
+                
+                if not any( i in newWord+letter for i in invalidCombinations ):
+                    breakMarker = True
+                    newWord += letter
+                    break
+                    
+        if breakMarker or currentRetries > maxRetries:
+            if currentRetries > maxRetries:
+                print 351513
+            break
+        currentRetries += 1
+                    
+
+print newWord.title()
+            
