7b15ca5912030bd81b25669bc3d600c1cd28eb23
Peter <Peter92@users.noreply.github.com>
2016-10-04 23:45:33 +0100

    Rewrote from scratch

--- NameGenerator.py
+++ NameGenerator.py
@@ -1,257 +1,235 @@
-import math, random, zlib, md5, os, cPickle, base64
-import marshal as cPickle
+from __future__ import division
+from string import ascii_lowercase, ascii_uppercase
+import random
 
-class LetterLoop:
-    letters = list( "abcdefghijklmnopqrstuvwxyz" )
-    
-    def __init__( self, start = 0 ):
-        self.currentNumber = start
-        
-    def next(self):
-        remainder = self.currentNumber
-        iteration = 1
-        iterationSize = len(self.letters)
-        
-        while remainder>=iterationSize:
-            remainder -= iterationSize
-            iterationSize *= len(self.letters)
-            iteration  += 1
-        result = ''
-        
-        for i in range( iteration ):
-            result = self.letters[remainder % len(self.letters)] + result
-            remainder /= len(self.letters)
-            
-        self.currentNumber += 1
-        
-        return result
-     
+def quick_round(i):
+    if i % 1 >= 0.5:
+        i += 1
+    return int(i)
 
-class NameGen:
+
+class WordGenerator(object):
+    def __init__(self, bank_size):
+        self.size = bank_size
+        self.cache = {}
     
-    cachePath = "NameGen.cache"
-    
-    def __init__( self, wordInput = [], accuracy = 15.0 ):
-        self.wordInput = wordInput
-        self.wordList = {j[0]:int( j[1] ) if j[1] else 1 for j in [[j[0],0] if len( j ) == 1 else j for j in [i.split( "," ) for i in self.wordInput]]}
-        self.accuracy = float( accuracy )
-    
-    #Read file for input formatting
-    def readFile( self ):
-        with open( self.wordInput ) as f:
-            return set( i.replace( "\r\n", "" ) for i in f.readlines() )
-  
-    
-    #Find all invalid combinations of letters
-    def invalidCombinations( self, depth = 3 ):
-        maxNumber = 0
-        for i in xrange( depth ):
-            maxNumber += 26**( depth-i )
+    def build(self, file_name, custom_name, line_end='\r\n', depth=2):
+        with open(file_name) as f:
+            contents = f.read()
+            file_hash = self._find_hash(contents)
+        
+        rebuild_invalid = custom_name if custom_name in self.cache else None
+        if custom_name not in self.cache:
+            hashes = {v['Hash']: k for k, v in self.cache.iteritems()}
             
-        return set( j for j in set( LetterLoop( i ).next() for i in xrange( maxNumber ) ) if j not in ",".join( self.wordList.keys() ) )
-    
-    def buildDictionary( self ):
+            #Reference existing value
+            if file_hash in hashes:
+                self.cache[custom_name] = {'Alias': hashes[file_hash],
+                                           'Hash': None,
+                                           'Words': None,
+                                           'WordBank': None,
+                                           'Invalid': None}
+                rebuild_invalid = hashes[file_hash]
+                    
+            #Create new values
+            else:
+                words = [i for i in contents.lower().split(line_end) if i]
+                self.cache[custom_name] = {'Alias': None,
+                                           'Hash': file_hash,
+                                           'Words': words,
+                                           'WordBank': self._build_dictionary(words),
+                                           'Invalid': {depth: self._build_invalid(words, depth)}}
+                rebuild_invalid = custom_name
         
-        #Iterate through each word to find the letter frequency
-        letterGroup = {letter: dict.fromkeys( xrange( int( self.accuracy ) ), 0 ) for letter in LetterLoop.letters}
-        letterDetailFrequency = {}
+        #Create new invalid depth if the requested one isn't already in the cache
+        if rebuild_invalid:
+            if depth not in self.cache[rebuild_invalid]['Invalid']:
+                self.cache[rebuild_invalid]['Invalid'][depth] = self._build_invalid(self.cache[rebuild_invalid]['Words'], depth)
+
+    def _find_hash(self, data):
+        """Leaving this here with a potential for a better hash."""
+        return hash(data)
+
+    def _build_dictionary(self, words):
         
-        for i in range( int( self.accuracy ) ):
-            letterDetailFrequency[i] = {}
+        bank = [{} for _ in xrange(self.size)] + [{}, 0]
+        
+        for word in words:
             
-        for wordToUse in self.wordList.keys():
-            wordFrequency = self.wordList[wordToUse]
-        
-            for letterToUse in LetterLoop.letters:
-        
-                #Find letter positions
-                letterIndex = [i for i, letter in enumerate( wordToUse ) if letter == letterToUse]
+            word_len = len(word)
+            index_start = 0
+            bank[-1] += 1
+            
+            try:
+                bank[-2][word_len] += 1
+            except KeyError:
+                bank[-2][word_len] = 1
+            
+            for i, character in enumerate(word):
                 
-                #Average over range of 0 to accuracy
-                letterAccuracy = self.accuracy/len( wordToUse )
-                accuracyIndex = [[i*letterAccuracy,( i+1 )*letterAccuracy] for i in letterIndex]
+                #Generate the start and end index for the bank
+                index_end = quick_round(((i + 1) / word_len) * (self.size - 1))
                 
-                #Find frequency of letters being used
-                for i in accuracyIndex:
-                    mainRange = range( int( math.floor( round( i[0], 5 ) ) ), int( math.ceil( round( i[1], 5 ) ) ) )
-                    for j in mainRange:
-                        startValue = i[0]
-                        if j > i[0]:
-                            startValue = j
-                        endValue = int( startValue )+1
-                        if endValue > i[1]:
-                            endValue = i[1]
-                        letterGroup[letterToUse][int( startValue )] += ( endValue-startValue )*wordFrequency
-            
-            
-            #Calculate frequency of each letter for different points
-            for i in range( int( self.accuracy ) ):
-                for letter in LetterLoop.letters:
-                    letterDetailFrequency[i][letter] = letterGroup[letter][i]
-    
-    
-        #Find average length of words:
-        allLengths = [len( word ) for word in self.wordList.keys()]
-        uniqueLengths = set( allLengths )
-        wordLenths = dict( ( i, allLengths.count( i ) ) for i in uniqueLengths )
-    
-        return letterDetailFrequency, wordLenths
-    
-    #Choose a length of a word
-    def chooseLength( self, wordLengths ):
-        wordLengthRandom = random.randint( 0, sum( wordLengths.values() ) )
-        wordLengthCount = 0
-        for newWordLength in wordLengths.keys():
-            wordLengthCount += wordLengths[newWordLength]
-            if wordLengthCount > wordLengthRandom:
-                return newWordLength
-    
-    
-    #Figure out which characters to make word from
-    def buildWord( self, newWordLength, letterDetailFrequency, invalidCombinations = [], printDebugInfo = 0 ):
-        
-        newWord = ""
-        letterAccuracy = self.accuracy/newWordLength
-        totalLoops = 0
-        
-        maxResets = 10 #Max times the last letter is removed before resetting
-        currentResets = 0
-        maxRetries = 10 #Max retries to find a new letter
-        
-        previousInvalidLetter = "~"
-        while len( newWord ) < newWordLength:
-            i = len( newWord )
-            totalLoops += 1
-            maxLoops = 50
-            if totalLoops > maxLoops:
-                if printDebugInfo > 0:
-                    print "Hit {0} loops, cancelling generation".format( maxLoops )
-                return None
+                #Write to the bank
+                for n in xrange(index_start, index_end + 1):
+                    try:
+                        bank[n][character] += 1
+                    except KeyError:
+                        try:
+                            bank[n][character] = 1
+                        except KeyError:
+                            bank[n] = {character: 1}
                 
-            minValue = i*letterAccuracy
-            maxValue = (i+1)*letterAccuracy
-            minWholeValue = int( math.ceil( round( minValue, 5 ) ) )
-            maxWholeValue = int( math.floor( round( maxValue, 5 ) ) )
+                index_start = index_end + 1
+        return bank
+
+    def _build_invalid(self, words, max_depth, _current_phrase='', _invalid=None, _current_depth=1):
+        results = set()
+        for letter in ascii_lowercase:
+            new_phrase = _current_phrase + letter
+            found = False
+            for word in words:
+                if new_phrase in word:
+                    found = True
+                    break
+            if found and _current_depth < max_depth:
+                results.update(self._build_invalid(words, max_depth, _current_phrase=new_phrase, _invalid=_invalid, _current_depth=_current_depth + 1))
+            if not found and new_phrase:
+                results.add(new_phrase)
+        return results
+        
+    def _generate_length(self, group_name, rnd=None):
+        
+        if rnd is None:
+            rnd = random
+        
+        bank = self.cache[group_name]['WordBank']
+        #word_index = QuickRandom().range(1, bank[-1]) - 1
+        word_index = rnd.randrange(0, bank[-1])
+        total = 0
+        for i in sorted(bank[-2].keys()):
+            total += bank[-2][i]
+            if total > word_index:
+                return i
+        raise ValueError('no word length found')
+
+
+    def generate_word(self, group_name, custom_word=None, seed=None, depth=2, max_retries=5):
+        
+        #Make random seed
+        current_seed = []
+        if custom_word is not None:
+            current_seed.append(custom_word)
+        if seed is not None:
+            current_seed.append(seed)
+        if current_seed:
+            rnd = random.Random(''.join(map(str, current_seed)))
+        else:
+            rnd = random
+        
+        if group_name not in self.cache:
+            raise ValueError('group {} doesn\'t exist'.format(group_name))
+        
+        if self.cache[group_name]['Alias'] is not None:
+            group_name = self.cache[group_name]['Alias']
+        bank = self.cache[group_name]['WordBank']
+        if depth not in self.cache[group_name]['Invalid']:
+            self.cache[group_name]['Invalid'][depth] = self._build_invalid(self.cache[group_name]['Words'], depth)
+        
+        size = self._generate_length(group_name, rnd)
+        if custom_word is not None:
+            word_size = len(custom_word)
+            size += word_size
+            size //= 2
+    
+        word_generation = ''
+        index_start = 0
+        for i in range(size):
+            index_end = quick_round(((i + 1) / size) * (self.size - 1))
             
+            retries = 0
             
-            #Calculate what precision bands the letter falls under
-            valueRange = xrange( minWholeValue, maxWholeValue )
-            individualLetterValues = {i:1 if i in valueRange else 0 for i in xrange( int( self.accuracy ) )}    
-            if minValue < minWholeValue:
-                individualLetterValues[int( minValue )] = minValue-int( minValue )
-            if maxValue > maxWholeValue:
-                individualLetterValues[int( maxValue )] = maxValue-int( maxValue )
-            
-            #Count all letters
-            combinedLetterValues = {letter:0 for letter in LetterLoop.letters}
-            for i in individualLetterValues.keys():
-                for j in combinedLetterValues.keys():
-                    combinedLetterValues[j] += letterDetailFrequency[i][j]*i
-            
-            #Build the word
-            currentRetries = 0
             while True:
                 
-                breakMarker = None
-                randomSelection = random.uniform( 0, sum( combinedLetterValues.values() ) )
-                i = 0
+                #Pick index of letter
+                num_letters = bank[-1] * (index_end - index_start + 1)
+                #letter_required = QuickRandom().range(0, num_letters)
+                letter_required = rnd.randint(0, num_letters)
                 
-                for letter in combinedLetterValues.keys():
+                #Find that letter
+                index_offset = max(0, letter_required - 1)
+                letter_required -= index_offset // bank[-1] * bank[-1]
+                index_current = index_start + index_offset // bank[-1]
                 
-                    #Get letter
-                    i += combinedLetterValues[letter]
-                    
-                    if i > randomSelection:
+                count = 0
+                for j in ascii_lowercase:
+                    try:
+                        if not bank[index_current][j]:
+                            raise KeyError()
+                    except KeyError:
+                        pass
+                    else:
+                        count += bank[index_current][j]
+                        if count >= letter_required:
+                            break
+                potential_new_word = word_generation + j
+                
+                #Check if any parts of the word are invalid
+                invalid = False
+                for j in range(len(potential_new_word)):
+                    if potential_new_word[-j:] in self.cache[group_name]['Invalid']:
+                        invalid = True
                         
-                        if not any( i in newWord+letter for i in invalidCombinations|set([previousInvalidLetter]) ):
-                            breakMarker = True
-                            newWord += letter
-                        break
-                
-                #Break out of while loop if letter is found
-                if breakMarker:
+                if not invalid:
+                    word_generation = potential_new_word
                     break
-                 
-                #Delete a letter and retry
-                if currentRetries and printDebugInfo > 2:
-                    print "Invalid last letter, tried to add {0} to {1}".format( letter, newWord )
                     
-                if currentRetries > maxRetries:
-                    
-                    #Fully reset word if maxResets has been hit
-                    if currentResets > maxResets:
-                        if printDebugInfo > 1:
-                            print "Retried building word {0} times, restarting the generation.".format( maxResets )
-                        newWord = ""
-                        break
-                        
-                    currentResets += 1
-                    currentRetries = -1
-                    previousInvalidLetter = newWord[-1:]
-                    newWord = newWord[:-1]
-                    
-                currentRetries += 1
-        
-        return newWord
-    
-    def build( self, depth = 3 ):
-        
-        #Fix for if someone inputs a filename
-        if type( self.wordInput ) not in [set, list]:
-            try:
-                wordInput = self.readFile()
-                return NameGen( wordInput ).build( depth )
-            except:
-                return None
+                if retries > max_retries:
+                    #print 'Failed to generate word, {} was not valid'.format(potential_new_word)
+                    if custom_word is None:
+                        custom_word = potential_new_word
+                    return self.generate_word(group_name, custom_word=custom_word + '0', seed=seed, depth=depth, max_retries=max_retries)
+                retries += 1
             
-        try:
-            depth = int( depth )
-        except:
-            depth = 2
+            index_start = index_end + 1
         
-        #Check input doesn't exist in cache
-        inputHash = md5.new()
-        inputHash.update( str( self.wordInput ) )
-        inputHash = inputHash.hexdigest()
-        
-        if os.path.exists( self.cachePath ):
-            try:
-                with open( self.cachePath, "r") as f:
-                    textFileData = self.decodeData( f.read() )
-            except:
-                textFileData = {}
-            textFile = open( self.cachePath, "r+")
-        else:
-            textFileData = {}
-            textFile = open( self.cachePath, "w")
-        
-        invalidCombinations = {}
-        if inputHash not in textFileData.keys():
-            letterData, lengthData = NameGen( self.wordList ).buildDictionary()
-            invalidCombinations[depth] = NameGen( self.wordList ).invalidCombinations( depth )
-        
-        elif depth not in textFileData[inputHash][2].keys():
-            letterData, lengthData = textFileData[inputHash][:2]
-            invalidCombinations = textFileData[inputHash][2]
-            invalidCombinations[depth] = NameGen( self.wordList ).invalidCombinations( depth )
-        
-        else:
-            letterData, lengthData = textFileData[inputHash][:2]
-            invalidCombinations = textFileData[inputHash][2]
-                
+        return word_generation
+    
+    def generate_name(self, *args):
+        return ' '.join(self.generate_word(i).capitalize() for i in args)
+    
+    def generate_sentence(self, group_name, sentence, seed=None):
+        result = []
+        for paragraph in (i.strip() for i in sentence.strip().replace('\r\n', '\r').replace('\n', '\r').split('\r')):
+            current_paragraph = []
+            for single_sentence in (i.strip() for i in paragraph.split('.')):
+                current_sentence = []
+                for phrase in (i.strip() for i in single_sentence.split(',')):
+                    current_phrase = []
+                    for word in phrase.split():
+                        if word[0] in ascii_uppercase and current_phrase:
+                            current_word = [word]
+                        else:
+                            current_word = []
+                            for split_word in word.split('-'):
+                                current_word.append(self.generate_word(group_name, custom_word=split_word, seed=seed))
+                        current_phrase.append('-'.join(current_word))
+                    current_sentence.append(' '.join(current_phrase))
+                    
+                current_sentence[0] = current_sentence[0][:1].capitalize() + current_sentence[0][1:]
+                current_paragraph.append(', '.join(current_sentence))
+            result.append('. '.join(current_paragraph))
+        return '\r\n'.join(result)
+try:
+    old_cache = a.cache
+except NameError:
+    word_gen = WordGenerator(40)
+else:
+    word_gen = WordGenerator(40)
+    word_gen.cache = old_cache
+    
+word_gen.build('C:/code/CSV_Database_of_First_Names.csv', 'names_en_first', depth=3)
+word_gen.build('C:/code/CSV_Database_of_Last_Names.csv', 'names_en_last')
 
-        textFileData[inputHash] = [letterData, lengthData, invalidCombinations]
-        
-        textFile.write( self.encodeData( textFileData ) )
-        textFile.close()
-        
-        newWordLength = self.chooseLength( lengthData )
-        return self.buildWord( newWordLength, letterData, invalidCombinations[depth] )
-    
-    def encodeData( self, input ):
-        return zlib.compress( cPickle.dumps( input ) )
-    
-    def decodeData( self, input ):
-        return cPickle.loads( zlib.decompress( input ) )
- 
-print " ".join( [NameGen( "C:/Code/CSV_Database_of_First_Names.csv" ).build(), NameGen( "C:/Code/CSV_Database_of_Last_Names.csv" ).build()] ).title()
+for i in range(10):
+    print word_gen.generate_name('names_en_first', 'names_en_last')
